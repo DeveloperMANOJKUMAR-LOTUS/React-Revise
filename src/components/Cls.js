628a9fc092ecc7d5cd7137ba7d261627a947bea2
Manojkumar <developermanoj2003@gmail.com>
2022-08-15 09:10:06 +0530

    func and cls comp

---
+++ src/components/Cls.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cls() {
+  return (
+    <div className='bg-dark p-3 text-danger container'>
+        <h3>Class Component ..</h3>
+        <p>Hai this is a class component . </p>
+    </div>
+  )
+}
+
+export default Cls
---
+++ src/components/FuncComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FuncComp() {
+  return (
+    <div className='container'>
+        <h4>Functional Component ..</h4>
+        <p className='bg-primary p-5 text-white'>This is a functional component .</p>
+    </div>
+  )
+}
+
+export default FuncComp
